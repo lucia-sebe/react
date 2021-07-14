f5f20fc9233e9f92421390c3d041899120db7fa3
lucia-sebe <luli.se04@gmail.com>
2021-07-14 15:02:40 -0300

    Agregando public y src

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header >
+        <
+        p >
+        Bienvenido!!!
+        <
+        /p> <
+        /header> <
+        /div>
+    );
+}
+
+export default App;
---
+++ src/api/movies.js
@@ -0,0 +1,4 @@
+export const getAllMovies = fetch('https://api.themoviedb.org/3/movie/popular?api_key=715b3c6bbd6ab40e34894c211f5e1bef%27')
+    .then((response) => {
+        return response.json()
+    })
