487e09c366ee58c93a30a3beb4a1b660c83a5c13
lucia-sebe <luli.se04@gmail.com>
2021-09-02 15:52:21 -0300

    usando Tailwind

--- src/components/highlight.jsx
+++ src/components/highlight.jsx
@@ -5,7 +5,6 @@ import { getMovieDetails } from '../api/movies';
 import Navbar from '../components/navbarHome';
 import '../styles/highlight.css';
 
-
 const Highlight = ({movie})  => {
     
     const [movieDetails , setMovieDetails] = useState (null);
@@ -20,13 +19,13 @@ const Highlight = ({movie})  => {
     return(
         <>
 
-            <div className="fondoHome" style={{ backgroundImage: `url(${IMAGE_URL}${movieDetails?.backdrop_path})` }}>
+            <div className="w-screen h-screen bg-cover" style={{ backgroundImage: `url(${IMAGE_URL}${movieDetails?.backdrop_path})` }}>
             <Navbar/>
-                <div className="movieDetails">
-                        <p id='dateAndGenres'>{movieDetails?.release_date.slice(0,4)} - {movieDetails?.genres.map((item) => `${item.name} `) }</p>
-                        <h1 id="movieTitle">{movieDetails?.title}</h1>
-                        <p id='runtime'>{movieDetails?.runtime} m</p>
-                        <p id='overview'>{movieDetails?.overview}</p>
+                <div className="movieDetails my-20 mx-80">
+                        <p className="dateAndGenres p-0">{movieDetails?.release_date.slice(0,4)} - {movieDetails?.genres.map((item) => `${item.name} `) }</p>
+                        <h1 className="text-5xl my-9">{movieDetails?.title}</h1>
+                        <p className="runtime w-16 p-2 rounded-lg">{movieDetails?.runtime} m</p>
+                        <p className="p-0 my-8 mx-0 text-4xl leading-normal">{movieDetails?.overview}</p>
                 </div>
             </div>
         </>
